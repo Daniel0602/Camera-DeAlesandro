9122233c2b41f721c145727f76adf37c3f58d339
Daniel0602 <109624996+Daniel0602@users.noreply.github.com>
2022-08-21 15:07:32 -0300

    Update Enemy1Movement.cs

--- Vectores DeAlesandro/Assets/Scripts/Enemy1Movement.cs
+++ Vectores DeAlesandro/Assets/Scripts/Enemy1Movement.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Enemy1Movement : MonoBehaviour
 {
-    Vector3 RelativePosition;
-    Quaternion targetRotation;
-
     Transform target;
+
     float speed;
+    public float speedRotation;
 
     void Update()
     {
-        RelativePosition = target.position - transform.position;
-        targetRotation = Quaternion.LookRotation(RelativePosition);
-        transform.rotation = Quaternion.Lerp(transform.rotation,targetRotation,speed * Time.deltaTime);
+        Vector3 Direction = transform.position - Target.position;
+        transform.LookAt(Target);
+        if (Direction.magnitude > 2)
+        {
+            transform.Translate(Direction * Speed * Time.deltaTime);
+        }
     }
 }
